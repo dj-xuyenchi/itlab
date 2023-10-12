e12d662ec1521f994c78ea6c948e9c5abc816059
doquanganh99zz <do.quanganh99zz@gmail.com>
2023-10-12 13:29:00 +0700

    rebase

--- fe-react/src/component/home/productdetail/ProductDetail.jsx
+++ fe-react/src/component/home/productdetail/ProductDetail.jsx
@@ -2,14 +2,58 @@ import { useSelector } from "react-redux";
 import "./style.css";
 import { selectLanguage } from "../../../language/selectLanguage";
 import Header from "../../common/header/Header";
-import Body from "../body/Body";
-import FotoramaSlider from "./FotoramaSlider";
+import ProductImgSlider from "./ProductImgSlider";
+import { fixMoney } from "../../../extensions/fixMoney";
 function ProductDetail() {
     const language = useSelector(selectLanguage);
     return (
         <>
             <Header />
-            <FotoramaSlider />
+            <div style={{
+                width: "96%",
+                marginLeft: "2%",
+                display: "flex",
+                marginTop: "24px"
+
+            }}>
+                <div style={{
+                    width: "40%"
+                }}>
+
+                    <ProductImgSlider />
+                </div>
+                <div style={{
+                    width: "58%",
+                    marginLeft: "2%"
+                }}>
+                    <div style={{
+                        marginTop: "20px",
+                        marginBottom: "20px",
+                    }}>
+                        <h1 style={{
+                            fontSize: "20px",
+                            fontWeight: 500,
+                            lineHeight: "24px",
+                            textTransform: "uppercase"
+                        }}>Áo Khoác Nam Flannel Tay Dài Khóa Kéo Kẻ Caro Form Regular - 10F23JAC002
+                        </h1>
+                    </div>
+                    <div style={{
+                        marginTop: "20px",
+                        marginBottom: "20px",
+                    }}>
+                        <span style={{
+                            fontWeight: 700,
+                            lineHeight: "28px",
+                            fontSize: "24px"
+                        }}>{fixMoney("1000000")}</span>
+                    </div>
+                    <div>
+                        <span>Chọn màu sắc:</span>
+
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
--- fe-react/src/component/home/productdetail/ProductImgSlider.jsx
+++ fe-react/src/component/home/productdetail/ProductImgSlider.jsx
@@ -1,7 +1,7 @@
 import Gallery from "react-image-gallery";
 import "./style.css";
 import 'react-image-gallery/styles/css/image-gallery.css';
-function FotoramaSlider() {
+function ProductImgSlider() {
     const images = [
         {
             original: 'https://routine.vn/media/catalog/product/cache/e78fcb931fd36e972f6051c94f188557/1/0/10f22jacw012_beige-ao-khoac-nu_4__2.jpg',
@@ -9,10 +9,6 @@ function FotoramaSlider() {
             description: 'Description 1',
         },
         {
-            original: 'https://routine.vn/media/catalog/product/cache/e78fcb931fd36e972f6051c94f188557/1/0/10f22jacw012_beige-ao-khoac-nu_4__2.jpg',
-            thumbnail: 'https://routine.vn/media/catalog/product/cache/e78fcb931fd36e972f6051c94f188557/1/0/10f22jacw012_beige-ao-khoac-nu_4__2.jpg',
-            description: 'Description 2',
-        }, {
             original: 'https://routine.vn/media/catalog/product/cache/e78fcb931fd36e972f6051c94f188557/1/0/10f22jacw012_beige-ao-khoac-nu_4__2.jpg',
             thumbnail: 'https://routine.vn/media/catalog/product/cache/e78fcb931fd36e972f6051c94f188557/1/0/10f22jacw012_beige-ao-khoac-nu_4__2.jpg',
             description: 'Description 2',
@@ -30,7 +26,7 @@ function FotoramaSlider() {
     );
 }
 
-export default FotoramaSlider;
+export default ProductImgSlider;
 
 
 
