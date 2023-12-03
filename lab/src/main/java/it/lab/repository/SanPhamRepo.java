3e98c2dfc17e7f8cf021cb1041d8c97c517c68b9
hungcbr250 <hungndkph28704@fpt.edu.vn>
2023-12-03 15:04:44 +0700

    update spsk service

--- lab/src/main/java/it/lab/repository/SanPhamRepo.java
+++ lab/src/main/java/it/lab/repository/SanPhamRepo.java
@@ -4,9 +4,13 @@ import it.lab.entity.SanPham;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SanPhamRepo extends JpaRepository<SanPham, Long> {
 
+    List<Object[]> getSanPhamE(int thang, int nam);
+
+    List<SanPham> getSanPhamTheoNhom(long nhom);
 }
