d5bcaa1f5e056b58ed94dd33980aaa5f36643faf
hungcbr250 <hungndkph28704@fpt.edu.vn>
2023-12-03 15:05:22 +0700

    update spsk

--- lab/src/main/java/it/lab/repository/SanPhamRepo.java
+++ lab/src/main/java/it/lab/repository/SanPhamRepo.java
@@ -2,6 +2,8 @@ package it.lab.repository;
 
 import it.lab.entity.SanPham;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +12,16 @@ import java.util.Optional;
 @Repository
 public interface SanPhamRepo extends JpaRepository<SanPham, Long> {
 
-    List<Object[]> getSanPhamE(int thang, int nam);
+    @Query("SELECT c.id,c.sanPham.tenSanPham, CONVERT(VARCHAR(100), c.hinhAnh) AS image, SUM(a.soLuong) AS tongSoLuong " +
+            "FROM HoaDonChiTiet a " +
+            "JOIN HoaDon b ON a.hoaDon.id = b.id " +
+            "JOIN SanPhamChiTiet c ON a.sanPhamChiTiet.id = c.id " +
+            "WHERE MONTH(b.ngayTao) = :thang " +
+            "AND YEAR(b.ngayTao) = :nam " +
+            "GROUP BY c.id, CONVERT(VARCHAR(100), c.hinhAnh), c.sanPham.tenSanPham " +
+            "ORDER BY SUM(a.soLuong) DESC")
+    List<Object[]> getSanPhamE(@Param("thang") int thang, @Param("nam") int nam);
 
-    List<SanPham> getSanPhamTheoNhom(long nhom);
+    @Query("select s from SanPham s where s.nhomSanPham.id = :id")
+    List<SanPham> getSanPhamTheoNhom(@Param("id") long id);
 }
