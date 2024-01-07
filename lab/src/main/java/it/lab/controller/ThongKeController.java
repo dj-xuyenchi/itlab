6c39c81d598ae64cd39a5019fafa528bd719ce6a
tiendung6529 <tiendung6529@gmail.com>
2024-01-07 19:11:01 +0700

    fix-thong-ke-bar
    fix-thong-ke-bar

--- lab/src/main/java/it/lab/controller/ThongKeController.java
+++ lab/src/main/java/it/lab/controller/ThongKeController.java
@@ -165,72 +165,10 @@ public class ThongKeController {
         return doanhThuTheoThang;
     }
 
-    //Thong ke 12 tháng bảng biểu đồ sau khi đã trừ giá nhập trừ giá bán
-//    @GetMapping("/bieu-do-tru-chi-phi")
-//    public List<BigDecimal> getBieuDoDataTruChiPhi(  @RequestParam(value = "startYear", defaultValue = "-1") int startYear,
-//                                                     @RequestParam(value = "endYear", defaultValue = "-1") int endYear) {
-//        int currentYear = YearMonth.now().getYear();
-//        List<BigDecimal> doanhThuTheoThangTruChiPhi = new ArrayList<>();
-//
-//        for (int month = 1; month <= 12; month++) {
-//            BigDecimal doanhThuThangTruChiPhi = repositoryHDCT.tinhTongDoanhThuNamSauKhiTruChiPhi(currentYear, month);
-//            doanhThuTheoThangTruChiPhi.add(doanhThuThangTruChiPhi);
-//        }
-//
-//        return doanhThuTheoThangTruChiPhi;
-//    }
-
-    //
-//    @GetMapping("/bieu-do-tong-hop")
-//    public Map<String, List<BigDecimal>> getBieuDoTongHop() {
-//        int currentYear = YearMonth.now().getYear();
-//
-//        List<BigDecimal> doanhThuTheoThang = new ArrayList<>();
-//        List<BigDecimal> doanhThuTheoThangTruChiPhi = new ArrayList<>();
-//
-//        for (int month = 1; month <= 12; month++) {
-//            BigDecimal doanhThuThang = repositoryThongKe.tinhTongDoanhThuTrongThangChar(currentYear, month);
-//            doanhThuTheoThang.add(doanhThuThang);
-//
-//            BigDecimal doanhThuThangTruChiPhi = repositoryHDCT.tinhTongDoanhThuNamSauKhiTruChiPhi(currentYear, month);
-//            doanhThuTheoThangTruChiPhi.add(doanhThuThangTruChiPhi);
-//        }
-//
-//        Map<String, List<BigDecimal>> result = new HashMap<>();
-//        result.put("doanhThuTheoThang", doanhThuTheoThang);
-//        result.put("doanhThuTheoThangTruChiPhi", doanhThuTheoThangTruChiPhi);
-//
-//        return result;
-//    }
-
-//    @GetMapping("/bieu-do-tong-hop")
-//    public Map<String, List<BigDecimal>> getBieuDoTongHop(
-//            @RequestParam(value = "year", defaultValue = "-1") int year) {
-//
-//        if (year == -1) {
-//            // If year is not specified in the URL, use the current year
-//            year = YearMonth.now().getYear();
-//        }
-//
-//        List<BigDecimal> doanhThuTheoThang = new ArrayList<>();
-//        List<BigDecimal> doanhThuTheoThangTruChiPhi = new ArrayList<>();
-//
-//        for (int month = 1; month <= 12; month++) {
-//            BigDecimal doanhThuThang = repositoryThongKe.tinhTongDoanhThuTrongThangChar(year, month);
-//            doanhThuTheoThang.add(doanhThuThang);
-//
-//            BigDecimal doanhThuThangTruChiPhi = repositoryHDCT.tinhTongDoanhThuNamSauKhiTruChiPhi(year, month);
-//            doanhThuTheoThangTruChiPhi.add(doanhThuThangTruChiPhi);
-//        }
-//
-//        Map<String, List<BigDecimal>> result = new HashMap<>();
-//        result.put("doanhThuTheoThang", doanhThuTheoThang);
-//        result.put("doanhThuTheoThangTruChiPhi", doanhThuTheoThangTruChiPhi);
-//
-//        return result;
-//    }
 
 
+
+//thongkebar
     @GetMapping("/bieu-do-tong-hop")
     public ResponseEntity<Map<Integer, Map<String, Map<String, BigDecimal>>>> calculateTotalProfitByYearAndMonth(
             @RequestParam(name = "startYear", required = false, defaultValue = "0") int startYear,
